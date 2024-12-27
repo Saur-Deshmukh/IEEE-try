b112422d0113a62fe184852f8b9a65c35d35d4ee
Saur-Deshmukh <162725317+Saur-Deshmukh@users.noreply.github.com>
2024-12-27 13:28:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:5173', // Allows all origins
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true 
